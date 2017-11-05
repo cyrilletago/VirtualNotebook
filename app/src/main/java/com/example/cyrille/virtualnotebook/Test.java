431ce4c4958844b2b1d46cd7721e7cbf7dd41b05
dominiquekogue <kedoswola@gmail.com>
2017-11-05 18:00:43 +0100

    test layout with bottom navigation

--- app/src/main/java/com/example/cyrille/virtualnotebook/Test.java
+++ app/src/main/java/com/example/cyrille/virtualnotebook/Test.java
@@ -1,8 +1,23 @@
 package com.example.cyrille.virtualnotebook;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+
 /**
  * Created by dominiquekogue on 11/3/17.
  */
 
-public class Test {
+public class Test extends AppCompatActivity {
+
+    CheckBox cb,cb2,cb3;
+
+    protected void appTest(){
+
+
+
+        cb = (CheckBox) findViewById(R.id.choice1);
+        cb2 = (CheckBox) findViewById(R.id.choice2);
+        cb3 = (CheckBox) findViewById(R.id.choice3);
+
+    }
 }
